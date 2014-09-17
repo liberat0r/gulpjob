84aaa2ecca53668d113273f6b49158f0ccc5ae02
liberat0r <lib3rat0r@gmail.com>
2014-09-18 01:01:55 +0300

    plugin vers

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 call npm install gulp --global
-call npm install gulp gulp-compass gulp-concat-sourcemap gulp-notify gulp-rename gulp-jshint gulp-watch gulp-plumber gulp-include map-stream browser-sync gulp-zip --save-dev
+call npm install gulp gulp-compass@1.2.0 gulp-concat-sourcemap gulp-notify gulp-rename gulp-jshint gulp-watch gulp-plumber gulp-include map-stream browser-sync@1.3.6 gulp-zip --save-dev
 call gulp -v
