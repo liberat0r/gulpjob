8bca81384ea82028655676727199aaf8e7f2807d
liberat0r <lib3rat0r@gmail.com>
2014-09-18 00:03:53 +0300

    install add gulp-zip

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 call npm install gulp --global
-call npm install gulp gulp-compass gulp-concat-sourcemap gulp-notify gulp-rename gulp-jshint gulp-watch gulp-plumber gulp-include map-stream browser-sync --save-dev
+call npm install gulp gulp-compass gulp-concat-sourcemap gulp-notify gulp-rename gulp-jshint gulp-watch gulp-plumber gulp-include map-stream browser-sync gulp-zip --save-dev
 call gulp -v
