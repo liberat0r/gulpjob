ed748b315655073190d615348d4ec71912b3cd38
liberat0r <lib3rat0r@gmail.com>
2014-09-17 09:38:06 +0300

    browsersync 1.3.6

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 call npm install gulp --global
-call npm install gulp gulp-uglifyjs gulp-compass@1.2.0 gulp-concat-sourcemap gulp-autoprefixer gulp-minify-css gulp-notify gulp-rename gulp-clean gulp-jshint gulp-imagemin imagemin-pngcrush gulp-watch gulp-plumber gulp-include map-stream browser-sync --save-dev
+call npm install gulp gulp-uglifyjs gulp-compass@1.2.0 gulp-concat-sourcemap gulp-autoprefixer gulp-minify-css gulp-notify gulp-rename gulp-clean gulp-jshint gulp-imagemin imagemin-pngcrush gulp-watch gulp-plumber gulp-include map-stream browser-sync@1.3.6 --save-dev
 call gulp -v
